62a6162c9c216569b70b8107be0693e3749ccd97
Manasa Mandadapu <m.manasa.mandadapu@accenture.com>
2020-02-20 15:54:09 +0530

    Updates

---
+++ precache-manifest.255ccd5165b5efba1dffb72a192a4cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2ec252ca50a2c8660555a0b32b7314",
+    "url": "/Reactapp-practice-smallgame/index.html"
+  },
+  {
+    "revision": "ee242d714fe2c2caa9c0",
+    "url": "/Reactapp-practice-smallgame/static/css/main.e49d7e2c.chunk.css"
+  },
+  {
+    "revision": "a0683a4ad777afecb65b",
+    "url": "/Reactapp-practice-smallgame/static/js/2.ab2ce748.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Reactapp-practice-smallgame/static/js/2.ab2ce748.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee242d714fe2c2caa9c0",
+    "url": "/Reactapp-practice-smallgame/static/js/main.3975cdb1.chunk.js"
+  },
+  {
+    "revision": "038ecbc1bbde8875116a",
+    "url": "/Reactapp-practice-smallgame/static/js/runtime-main.c53dffb9.js"
+  }
+]);
